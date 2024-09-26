8204fe175337ad3334eda83341d5f3746ac6eaca
rdrigo20 <rdrigo20@gmail.com>
2024-09-26 12:20:00 -0300

    feito o css e ajeitado o controle

--- controle.php
+++ controle.php
@@ -13,6 +13,7 @@ if (!isset($_SESSION['nome_usuario'])) {
 <head>
     <meta charset="UTF-8">
     <title>Painel de Controle</title>
+    <link rel="stylesheet" href="style.css"> <!-- Referencia o CSS -->
     <!-- framework da bootstrap --> 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
@@ -31,16 +32,18 @@ if (!isset($_SESSION['nome_usuario'])) {
         </a>
     </header>
 
-    <h2>Painel de Controle</h2>
-    <p>Bem-vindo, <?= $_SESSION['nome_usuario']; ?>!</p>
-    
-    <!-- Botão para criar novos registros -->
-    <button onclick="window.location.href='criar.php'">Área de Criar</button>
+    <center>
+        <h2>Painel de Controle</h2>
+        <p>Bem-vindo, <?= $_SESSION['nome_usuario']; ?>!</p>
+        
+        <!-- Botão para criar novos registros -->
+        <button onclick="window.location.href='criar.php'">Área de Criar</button>
 
-    <!-- Botão para visualizar (pegar) registros -->
-    <button onclick="window.location.href='pegar.php'">Área de Pegar</button>
+        <!-- Botão para visualizar (pegar) registros -->
+        <button onclick="window.location.href='pegar.php'">Área de Pegar</button>
 
-    <br><br>
-    <a href="logout.php"><button>Logout</button></a>
+        <br><br>
+        <a href="logout.php"><button>Logout</button></a>
+    </center>
 </body>
 </html>
